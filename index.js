59c79e483faea7f0c233427f5aabb233414d09d1
Maria T. Andersen <mteilmann93@gmail.com>
2020-11-15 13:48:08 +0100

    add more comments to code

--- index.js
+++ index.js
@@ -8,12 +8,15 @@ const groceryList = document.getElementById("groceries");
 /* Defining input item / new item on grocery list */
 const inputItem = document.getElementById("input");
 
+/* The eventlistener underneath adds a new element to list according to what is typed into input field*/
+submitBtn.addEventListener("click", function () {
+  let item = document.createElement("li");
+  item.innerText = inputItem.value;
+  groceryList.appendChild(item);
+});
 
-
-submitBtn.addEventListener("click", function() {
-    
-    let item = document.createElement("li");
-    item.innerText = inputItem.value;
-    groceryList.appendChild(item);
+/* The eventlistener underneath clears grocery list completely */
+clearBtn.addEventListener("click", function () {
+  groceryList.innerHTML = "";
 });
 
